39a70230b6878467b11bbe519368129dc7f52395
Artem <artknutov@gmail.com>
2022-06-24 14:03:22 +0300

    first commit

---
+++ src/main/java/com/incircle/IncircleApplication.java
@@ -0,0 +1,13 @@
+package com.incircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incircle/IncircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
