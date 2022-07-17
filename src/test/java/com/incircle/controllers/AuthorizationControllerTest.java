c10ae826ec82f851d0860212ea19eefff28c1305
Artem <artknutov@gmail.com>
2022-07-17 05:45:29 +0300

    added junit5 login tests

---
+++ src/test/java/com/incircle/controllers/AuthorizationControllerTest.java
@@ -0,0 +1,52 @@
+package com.incircle.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthorizationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Краткое описание сервиса")));
+    }
+
+    @Test
+    public void loginIsAvailable() throws Exception {
+        mockMvc
+                .perform(get("/login"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void goodLogin() throws Exception {
+        mockMvc
+                .perform(formLogin("/login").user("ar").password("12"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/contacts"));
+    }
+
+    @Test
+    void badLogin() throws Exception {
+        mockMvc
+                .perform(formLogin("/login").user("ar").password("11"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
+    }
+
+}
