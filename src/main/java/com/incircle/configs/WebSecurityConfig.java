44b37582eeb0e1b11e6e969f3a000ee9b6cd16bf
mrserg86 <84500437+mrserg86@users.noreply.github.com>
2022-08-21 21:53:32 +0300

    Favicon

--- src/main/java/com/incircle/configs/WebSecurityConfig.java
+++ src/main/java/com/incircle/configs/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .authorizeRequests()
                     .antMatchers("/register", "/login").anonymous()
-                    .antMatchers("/", "/webjars/**", "/img/circle.png", "/css/main.css").permitAll()
+                    .antMatchers("/", "/webjars/**", "/static/favicon.ico", "/img/circle.png", "/css/main.css").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
